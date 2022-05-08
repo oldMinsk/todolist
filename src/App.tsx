dc10183d80f3ac7600d96ebc5cb36035628b7024
oldMinsk <yuriss123@gmail.com>
2022-05-08 21:13:12 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ToDoList from "./ToDoList";
+
+function App() {
+    return (
+        <div className="App">
+            <ToDoList />
+            <ToDoList />
+            <ToDoList />
+        </div>
+    );
+}
+
+export default App;
